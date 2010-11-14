6dd903af1c5ab29349093492b171d0aa8e979a53
gcross <gcross@phys.washington.edu>
2010-11-14 13:59:26 -0800

    Added tests of label for the List monad.

--- tests.hs
+++ tests.hs
@@ -57,6 +57,49 @@ main = defaultMain
             ]
         -- @nonl
         -- @-node:gcross.20101114125204.1277:Maybe
+        -- @+node:gcross.20101114125204.1285:List
+        ,testGroup "List"
+            -- @    @+others
+            -- @+node:gcross.20101114125204.1286:Nothing
+            [testCase "[]" $ [] @=? label (visit ([] :: [Int]))
+            -- @nonl
+            -- @-node:gcross.20101114125204.1286:Nothing
+            -- @+node:gcross.20101114125204.1287:Single branch
+            ,testGroup "Single branch"
+                [testCase (show a ++ " `mplus` " ++ show b) $
+                    c @=? label (visit a `mplus` visit b)
+                | (a,b,c) ←
+                    [([1],[2],[(Seq.singleton L,1),(Seq.singleton R,2)])
+                    ,([1,2],[3,4],[(Seq.singleton L,1),(Seq.singleton L,2),(Seq.singleton R,3),(Seq.singleton R,4)])
+                    ]
+                ]
+            -- @nonl
+            -- @-node:gcross.20101114125204.1287:Single branch
+            -- @+node:gcross.20101114125204.1288:Two branches
+            ,testGroup "Two branches"
+                -- @    @+others
+                -- @+node:gcross.20101114125204.1289:#1
+                [testCase "#1" $
+                    [(Seq.fromList [L,L],(0,0))
+                    ,(Seq.fromList [L,R],(0,1))
+                    ,(Seq.fromList [R,L],(1,0))
+                    ,(Seq.fromList [R,R],(1,1))
+                    ]
+                    @=?
+                    label
+                    (do x ← (return 0) `mplus` (return 1)
+                        y ← (return 0) `mplus` (return 1)
+                        return (x,y)
+                    )
+                -- @nonl
+                -- @-node:gcross.20101114125204.1289:#1
+                -- @-others
+                ]
+            -- @-node:gcross.20101114125204.1288:Two branches
+            -- @-others
+            ]
+        -- @nonl
+        -- @-node:gcross.20101114125204.1285:List
         -- @-others
         ]
     -- @nonl
