5a8e1bd2f4c4a3e5f559abca3e8104e3a9725be3
Gregory Crosswhite <gcrosswhite@gmail.com>
2011-07-22 15:25:16 -0400

    Rewrote to enable distributed visitation.

--- Control/Monad/Trans/Visitor.hs
+++ Control/Monad/Trans/Visitor.hs
@@ -1,9 +1,12 @@
 -- @+leo-ver=5-thin
 -- @+node:gcross.20101114125204.1255: * @file Control/Monad/Trans/Visitor.hs
--- @@language Haskell
+-- @@language haskell
 
 -- @+<< Language extensions >>
 -- @+node:gcross.20101114125204.1261: ** << Language extensions >>
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE UnicodeSyntax #-}
 -- @-<< Language extensions >>
 
@@ -11,63 +14,56 @@ module Control.Monad.Trans.Visitor where
 
 -- @+<< Import needed modules >>
 -- @+node:gcross.20101114125204.1256: ** << Import needed modules >>
-import Control.Arrow (second)
-import Control.Monad (MonadPlus(..))
+import Control.Monad (MonadPlus(..),(>=>),liftM2)
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Operational (ProgramT,ProgramViewT(..),singleton,viewT)
+import Control.Monad.Trans.Class (MonadTrans)
 
-import qualified Data.Sequence as Seq
-import Data.Sequence (Seq,ViewL(EmptyL,(:<)),(|>),viewl)
+import Data.Functor.Identity (Identity,runIdentity)
+import Data.Serialize (Serialize())
 -- @-<< Import needed modules >>
 
 -- @+others
 -- @+node:gcross.20101114125204.1257: ** Types
--- @+node:gcross.20101114125204.1293: *3* Branch
-type Branch = Seq Choice
--- @+node:gcross.20101114125204.1265: *3* Choice
-data Choice = L | R deriving (Eq,Ord,Enum,Read,Show)
--- @+node:gcross.20101114125204.1258: *3* VisitorT
-newtype VisitorT v m α = VisitorT { runVisitorT :: (v → Choice → Maybe v) → v → m (v,α) }
+-- @+node:gcross.20101114125204.1258: *3* VisitorInstruction
+data VisitorInstruction m α where
+    Cache :: Serialize α ⇒ m α → VisitorInstruction m α
+    Choice :: VisitorT m α → VisitorT m α → VisitorInstruction m α
+    IsFirstVisit :: VisitorInstruction m Bool
+    Null :: VisitorInstruction m α
+-- @+node:gcross.20110722110408.1165: *3* VisitorT
+type VisitorT m = ProgramT (VisitorInstruction m) m
+-- @+node:gcross.20110722110408.1168: *3* Visitor
+type Visitor = VisitorT Identity
 -- @+node:gcross.20101114125204.1268: ** Instances
--- @+node:gcross.20101114125204.1270: *3* Functor VisitorT
-instance Functor m ⇒ Functor (VisitorT v m) where
-    fmap f mx = VisitorT $ \descend v → fmap (second f) (runVisitorT mx descend v)
--- @+node:gcross.20101114125204.1269: *3* Monad VisitorT
-instance Monad m ⇒ Monad (VisitorT v m) where
-    return x = VisitorT $ \_ v → return (v,x)
-    mx >>= f =
-        VisitorT $ \descend v → do
-            (v,x) ← runVisitorT mx descend v
-            runVisitorT (f x) descend v
-    mx >> my =
-        VisitorT $ \descend v → do
-            (v,_) ← runVisitorT mx descend v
-            runVisitorT my descend v
 -- @+node:gcross.20101114125204.1271: *3* MonadPlus VisitorT
-instance MonadPlus m ⇒ MonadPlus (VisitorT v m) where
-    mzero = VisitorT $ \_ _ → mzero
-    mx `mplus` my = VisitorT $ \descend v →
-        case (descend v L,descend v R) of
-            (Just v1,Just v2) →
-                (runVisitorT mx descend v1)
-                `mplus`
-                (runVisitorT my descend v2)
-            (Just v,Nothing) → runVisitorT mx descend v
-            (Nothing, Just v) → runVisitorT my descend v
-            (Nothing,Nothing) → mzero
--- @+node:gcross.20101114125204.1273: ** Functions
--- @+node:gcross.20101114125204.1274: *3* label
-label :: VisitorT Branch m α → m (Branch,α)
-label v = runVisitorT v (\v b → Just (v |> b)) Seq.empty
--- @+node:gcross.20101114125204.1276: *3* visit
-visit :: Monad m ⇒ m α → VisitorT v m α
-visit mx = VisitorT $ \_ v → mx >>= return . ((,) v)
--- @+node:gcross.20101114125204.1295: *3* walkDownBranch
-walkDownBranch :: Functor m ⇒ Branch → VisitorT Branch m α → m α
-walkDownBranch branch v = fmap snd (runVisitorT v descend branch)
-  where
-    descend branch choice =
-        case viewl branch of
-            EmptyL → Just branch
-            head :< tail | choice == head → Just tail
-            _ → Nothing
+instance Monad m ⇒ MonadPlus (VisitorT m) where
+    mzero = singleton Null
+    left `mplus` right = singleton $ Choice left right
+-- @+node:gcross.20110722110408.1166: ** Functions
+-- @+node:gcross.20110722110408.1180: *3* cache
+cache :: (Monad m, Serialize x) ⇒ x → VisitorT m x
+cache = singleton . Cache . return
+-- @+node:gcross.20110722110408.1170: *3* gatherResults
+gatherResults :: Visitor α → [α]
+gatherResults = runIdentity . runAndGatherResults
+-- @+node:gcross.20110722110408.1172: *3* isFirstVisit
+isFirstVisit :: VisitorT m Bool
+isFirstVisit = singleton IsFirstVisit
+-- @+node:gcross.20110722110408.1171: *3* runAndCache
+runAndCache :: Serialize x ⇒ m x → VisitorT m x
+runAndCache = singleton . Cache
+-- @+node:gcross.20110722110408.1167: *3* runAndGatherResults
+runAndGatherResults :: Monad m ⇒ VisitorT m α → m [α]
+runAndGatherResults = viewT >=> \view →
+    case view of
+        Return x → return [x]
+        (Cache mx :>>= k) → mx >>= runAndGatherResults . k
+        (Choice left right :>>= k) →
+            liftM2 (++)
+                (runAndGatherResults $ left >>= k)
+                (runAndGatherResults $ right >>= k)
+        (IsFirstVisit :>>= k) → runAndGatherResults $ k True
+        (Null :>>= _) → return []
 -- @-others
 -- @-leo
--- tests.hs
+++ tests.hs
@@ -1,6 +1,6 @@
 -- @+leo-ver=5-thin
 -- @+node:gcross.20101114125204.1259: * @file tests.hs
--- @@language Haskell
+-- @@language haskell
 
 -- @+<< Language extensions >>
 -- @+node:gcross.20101114125204.1281: ** << Language extensions >>
@@ -9,10 +9,7 @@
 
 -- @+<< Import needed modules >>
 -- @+node:gcross.20101114125204.1260: ** << Import needed modules >>
-import Control.Arrow
 import Control.Monad
-
-import qualified Data.Sequence as Seq
 
 import Test.Framework
 import Test.Framework.Providers.HUnit
@@ -28,121 +25,19 @@ main = defaultMain
     -- @+<< Tests >>
     -- @+node:gcross.20101114125204.1267: ** << Tests >>
     -- @+others
-    -- @+node:gcross.20101114125204.1275: *3* label
-    [testGroup "label"
+    -- @+node:gcross.20110722110408.1173: *3* gatherResults
+    [testGroup "gatherResults"
         -- @+others
-        -- @+node:gcross.20101114125204.1277: *4* Maybe
-        [testGroup "Maybe"
-            -- @+others
-            -- @+node:gcross.20101114125204.1278: *5* Nothing
-            [testCase "Nothing" $ Nothing @=? label (visit (Nothing :: Maybe Int))
-            -- @+node:gcross.20101114125204.1280: *5* Single choice
-            ,testGroup "Single choice"
-                [testCase (show a ++ " `mplus` " ++ show b) $
-                    c @=? label (visit a `mplus` visit b)
-                | (a,b,c) ←
-                    [(Just 1,Just 2,Just (Seq.singleton L,1))
-                    ,(Just 1,Nothing,Just (Seq.singleton L,1))
-                    ,(Nothing,Just 2,Just (Seq.singleton R,2))
-                    ]
-                ]
-            -- @-others
-            ]
-        -- @+node:gcross.20101114125204.1285: *4* List
-        ,testGroup "List"
-            -- @+others
-            -- @+node:gcross.20101114125204.1286: *5* Nothing
-            [testCase "[]" $ [] @=? label (visit ([] :: [Int]))
-            -- @+node:gcross.20101114125204.1287: *5* Single choice
-            ,testGroup "Single choice"
-                [testCase (show a ++ " `mplus` " ++ show b) $
-                    c @=? label (visit a `mplus` visit b)
-                | (a,b,c) ←
-                    [([1],[2],[(Seq.singleton L,1),(Seq.singleton R,2)])
-                    ,([1,2],[3,4],[(Seq.singleton L,1),(Seq.singleton L,2),(Seq.singleton R,3),(Seq.singleton R,4)])
-                    ]
-                ]
-            -- @+node:gcross.20101114125204.1288: *5* Two choices
-            ,testGroup "Two choices"
-                -- @+others
-                -- @+node:gcross.20101114125204.1289: *6* #1
-                [testCase "#1" $
-                    [(Seq.fromList [L,L],(0,0))
-                    ,(Seq.fromList [L,R],(0,1))
-                    ,(Seq.fromList [R,L],(1,0))
-                    ,(Seq.fromList [R,R],(1,1))
-                    ]
-                    @=?
-                    label
-                    (do x ← (return 0) `mplus` (return 1)
-                        y ← (return 0) `mplus` (return 1)
-                        return (x,y)
-                    )
-                -- @-others
-                ]
-            -- @-others
-            ]
-        -- @-others
-        ]
-    -- @+node:gcross.20101114125204.3782: *3* walkDownBranch
-    ,testGroup "walkDownBranch"
-        -- @+others
-        -- @+node:gcross.20101114125204.3783: *4* Maybe
-        [testGroup "Maybe"
-            -- @+others
-            -- @+node:gcross.20101114125204.3784: *5* Nothing
-            [testCase "Nothing" $ Nothing @=? walkDownBranch Seq.empty (visit (Nothing :: Maybe Int))
-            -- @+node:gcross.20101114125204.3785: *5* Single choice
-            ,testGroup "Single choice"
-                [testCase (show a ++ " `mplus` " ++ show b) $
-                    d @=? walkDownBranch c (visit a `mplus` visit b)
-                | (a,b,c,d) ←
-                    [(Just 1,Just 2,Seq.singleton L,Just 1)
-                    ,(Just 1,Just 2,Seq.singleton R,Just 2)
-                    ,(Just 1,Nothing,Seq.singleton L,Just 1)
-                    ,(Just 1,Nothing,Seq.singleton R,Nothing)
-                    ,(Nothing,Just 2,Seq.singleton L,Nothing)
-                    ,(Nothing,Just 2,Seq.singleton R,Just 2)
-                    ]
-                ]
-            -- @-others
-            ]
-        -- @+node:gcross.20101114125204.3786: *4* List
-        ,testGroup "List"
-            -- @+others
-            -- @+node:gcross.20101114125204.3787: *5* Nothing
-            [testCase "[]" $ [] @=? walkDownBranch Seq.empty (visit ([] :: [Int]))
-            -- @+node:gcross.20101114125204.3788: *5* Single choice
-            ,testGroup "Single choice"
-                [testCase (show a ++ " `mplus` " ++ show b) $
-                    d @=? walkDownBranch c (visit a `mplus` visit b)
-                | (a,b,c,d) ←
-                    [([1],[2],Seq.fromList [L,L],[1])
-                    ,([1],[2],Seq.fromList [L,R],[1])
-                    ,([1],[2],Seq.fromList [R,L],[2])
-                    ,([1],[2],Seq.fromList [R,R],[2])
-                    ,([1,2],[3,4],Seq.fromList [L,L,L],[1,2])
-                    ,([1,2],[3,4],Seq.fromList [R,R,R],[3,4])
-                    ]
-                ]
-            -- @+node:gcross.20101114125204.3789: *5* Two choices
-            ,testGroup "Two choices" $
-                let m = do  x ← (return 0) `mplus` (return 1)
-                            y ← (return 0) `mplus` (return 1)
-                            return (x,y)
-                in
-                [testCase (show branch) $ correct @=? walkDownBranch branch m
-                | (branch,correct) ← map (first Seq.fromList)
-                    [([L],[(0,0),(0,1)])
-                    ,([R],[(1,0),(1,1)])
-                    ,([L,L],[(0,0)])
-                    ,([L,R],[(0,1)])
-                    ,([R,L],[(1,0)])
-                    ,([R,R],[(1,1)])
-                    ]
-                ]
-            -- @-others
-            ]
+        -- @+node:gcross.20110722110408.1177: *4* return
+        [testCase "return" $ gatherResults (return ()) @?= [()]
+        -- @+node:gcross.20110722110408.1174: *4* mzero
+        ,testCase "mzero" $ gatherResults (mzero :: Visitor ()) @?= []
+        -- @+node:gcross.20110722110408.1178: *4* mplus
+        ,testCase "mplus" $ gatherResults (return 1 `mplus` return 2) @?= [1,2]
+        -- @+node:gcross.20110722110408.1179: *4* cache
+        ,testCase "cache" $ gatherResults (cache 42) @?= [42::Int]
+        -- @+node:gcross.20110722110408.1181: *4* isFirstVisit
+        ,testCase "isFirstVisit" $ gatherResults isFirstVisit @?= [True]
         -- @-others
         ]
     -- @-others
