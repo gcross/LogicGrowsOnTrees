d599d26c6162cd0a02399ce91f5851929a2fff60
Gregory Crosswhite <gcrosswhite@gmail.com>
2011-09-25 20:05:43 +1000

    walkVisitor --> walkVisitorT;  added walkVisitor for Identity monad.

--- Control/Monad/Trans/Visitor/Path.hs
+++ Control/Monad/Trans/Visitor/Path.hs
@@ -19,6 +19,7 @@ import Control.Monad ((>=>))
 import Control.Monad.Operational (ProgramViewT(..),viewT)
 
 import Data.ByteString (ByteString)
+import Data.Functor.Identity (runIdentity)
 import Data.Sequence (Seq,viewl,ViewL(..))
 import Data.Serialize (Serialize(),decode)
 import Data.Typeable (Typeable)
@@ -43,18 +44,21 @@ data WalkError =
 
 instance Exception WalkError
 -- @+node:gcross.20110923120247.1208: ** Functions
--- @+node:gcross.20110923120247.1207: *3* walkVisitor
-walkVisitor :: Monad m ⇒ VisitorPath → VisitorT m α → m (VisitorT m α)
-walkVisitor (viewl → EmptyL) = return
-walkVisitor path@(viewl → step :< tail) =
+-- @+node:gcross.20110923164140.1190: *3* walkVisitor
+walkVisitor :: VisitorPath → Visitor α → Visitor α
+walkVisitor path = runIdentity . walkVisitorT path
+-- @+node:gcross.20110923120247.1207: *3* walkVisitorT
+walkVisitorT :: Monad m ⇒ VisitorPath → VisitorT m α → m (VisitorT m α)
+walkVisitorT (viewl → EmptyL) = return
+walkVisitorT path@(viewl → step :< tail) =
     viewT >=> \view → case (view,step) of
         (Return x,_) → throw DeadEnd
         (Null :>>= _,_) → throw DeadEnd
-        (IsFirstVisit :>>= k,_) → walkVisitor path $ k False
-        (Cache _ :>>= k,CacheStep cache) → walkVisitor tail $ either error k (decode cache)
+        (IsFirstVisit :>>= k,_) → walkVisitorT path $ k False
+        (Cache _ :>>= k,CacheStep cache) → walkVisitorT tail $ either error k (decode cache)
         (Cache _ :>>= _,ChoiceStep _) → throw ChoiceStepAtCachePoint
-        (Choice left _ :>>= k,ChoiceStep False) → walkVisitor tail (left >>= k)
-        (Choice _ right :>>= k,ChoiceStep True) → walkVisitor tail (right >>= k)
+        (Choice left _ :>>= k,ChoiceStep False) → walkVisitorT tail (left >>= k)
+        (Choice _ right :>>= k,ChoiceStep True) → walkVisitorT tail (right >>= k)
         (Choice _ _ :>>= _,CacheStep _) → throw CacheStepAtChoicePoint
 -- @-others
 -- @-leo
