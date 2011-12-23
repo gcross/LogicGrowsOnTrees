40b5629298cfb1eb8bcc2ca4f2089fc0a16e1c30
Gregory Crosswhite <gcrosswhite@gmail.com>
2011-12-23 16:04:04 +1000

    Moved the generic reactive combinators and functions into a separate module.

---
+++ Control/Monad/Trans/Visitor/Reactive.hs
@@ -0,0 +1,59 @@
+-- @+leo-ver=5-thin
+-- @+node:gcross.20111223134617.1431: * @file Control/Monad/Trans/Visitor/Reactive.hs
+-- @@language haskell
+
+-- @+<< Language extensions >>
+-- @+node:gcross.20111223134617.1432: ** << Language extensions >>
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UnicodeSyntax #-}
+-- @-<< Language extensions >>
+
+module Control.Monad.Trans.Visitor.Reactive where
+
+-- @+<< Import needed modules >>
+-- @+node:gcross.20111223134617.1433: ** << Import needed modules >>
+import Control.Arrow ((&&&))
+import Control.Monad.IO.Class
+
+import Data.Either.Unwrap (fromLeft,fromRight,isLeft,isRight)
+import Data.Maybe (fromJust,isJust)
+
+import Reactive.Banana (fromAddHandler,newAddHandler)
+import Reactive.Banana.Model
+-- @-<< Import needed modules >>
+
+-- @+others
+-- @+node:gcross.20111223134617.1434: ** Functions
+-- @+node:gcross.20111219132352.1428: *3* (<$?>)
+infixl 4 <$?>
+
+(<$?>) :: FRP ξ ⇒ (a → Maybe b) → Event ξ a → Event ξ b
+f <$?> x = filterJust (f <$> x)
+-- @+node:gcross.20111219132352.1427: *3* (<$↔>)
+infixl 4 <$↔>
+
+(<$↔>) :: FRP ξ ⇒ (a → Either b c) → Event ξ a → (Event ξ b,Event ξ c)
+f <$↔> x = split (f <$> x)
+-- @+node:gcross.20111223134617.1427: *3* (<@?>)
+infixl 4 <@?>
+
+(<@?>) :: (FRP ξ, Apply f (Event ξ)) ⇒ f (a → Maybe b) → Event ξ a → Event ξ b
+f <@?> x = filterJust (f <@> x)
+-- @+node:gcross.20111026220221.1283: *3* (<@↔>)
+infixl 4 <@↔>
+
+(<@↔>) :: (FRP ξ, Apply f (Event ξ)) ⇒ f (a → Either b c) → Event ξ a → (Event ξ b,Event ξ c)
+f <@↔> x = split (f <@> x)
+-- @+node:gcross.20111026213013.1279: *3* filterJust
+filterJust :: FRP ξ ⇒ Event ξ (Maybe a) → Event ξ a
+filterJust = fmap fromJust . filterE isJust
+-- @+node:gcross.20111026213013.1281: *3* newHandler
+newHandler = do
+    (event_handler,callback) ← liftIO newAddHandler
+    event ← fromAddHandler event_handler
+    return (event,callback)
+-- @+node:gcross.20111026213013.1282: *3* split
+split :: FRP ξ ⇒ Event ξ (Either a b) → (Event ξ a,Event ξ b)
+split = (fmap fromLeft . filterE isLeft) &&& (fmap fromRight . filterE isRight)
+-- @-others
+-- @-leo
--- Control/Monad/Trans/Visitor/Reactive/Worker.hs
+++ Control/Monad/Trans/Visitor/Reactive/Worker.hs
@@ -5,7 +5,6 @@
 -- @+<< Language extensions >>
 -- @+node:gcross.20111026172030.1275: ** << Language extensions >>
 {-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE Rank2Types #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -19,7 +18,7 @@ module Control.Monad.Trans.Visitor.Reactive.Worker where
 
 -- @+<< Import needed modules >>
 -- @+node:gcross.20111026172030.1276: ** << Import needed modules >>
-import Control.Arrow ((&&&),second)
+import Control.Arrow (second)
 import Control.Exception (Exception(..),SomeException)
 import Control.Concurrent (killThread)
 import Control.Monad ((>=>),unless)
@@ -28,10 +27,8 @@ import Control.Monad.Tools (whenM,unlessM)
 
 import Data.Derive.Monoid
 import Data.DeriveTH
-import Data.Either.Unwrap (fromLeft,fromRight,isLeft,isRight)
 import Data.IORef (IORef,atomicModifyIORef,newIORef,readIORef,writeIORef)
 import qualified Data.IVar as IVar
-import Data.Maybe (fromJust,isJust)
 import Data.Monoid (Monoid(..))
 import Data.Monoid.Unicode ((⊕))
 import Data.Sequence ((|>))
@@ -40,6 +37,7 @@ import Reactive.Banana
 
 import Control.Monad.Trans.Visitor
 import Control.Monad.Trans.Visitor.Checkpoint
+import Control.Monad.Trans.Visitor.Reactive
 import Control.Monad.Trans.Visitor.Worker
 import Control.Monad.Trans.Visitor.Workload
 -- @-<< Import needed modules >>
@@ -74,26 +72,6 @@ data VisitorWorkerReactiveRequest =
     StatusUpdateReactiveRequest
   | WorkloadStealReactiveRequest
 -- @+node:gcross.20111026172030.1281: ** Functions
--- @+node:gcross.20111219132352.1428: *3* (<$?>)
-infixl 4 <$?>
-
-(<$?>) :: (a → Maybe b) → Event a → Event b
-(<$?>) f = filterJust . fmap f
--- @+node:gcross.20111219132352.1427: *3* (<$↔>)
-infixl 4 <$↔>
-
-(<$↔>) :: (a → Either b c) → Event a → (Event b,Event c)
-f <$↔> x = split (f <$> x)
--- @+node:gcross.20111223134617.1427: *3* (<@?>)
-infixl 4 <@?>
-
-(<@?>) :: Apply f Event ⇒ f (a → Maybe b) → Event a → Event b
-f <@?> x = filterJust (f <@> x)
--- @+node:gcross.20111026220221.1283: *3* (<@↔>)
-infixl 4 <@↔>
-
-(<@↔>) :: Apply f Event ⇒ f (a → Either b c) → Event a → (Event b,Event c)
-f <@↔> x = split (f <@> x)
 -- @+node:gcross.20111117140347.1433: *3* createVisitorIOWorkerReactiveNetwork
 createVisitorIOWorkerReactiveNetwork ::
     WorkerIncomingEvents →
@@ -118,9 +96,6 @@ createVisitorWorkerReactiveNetwork ::
 createVisitorWorkerReactiveNetwork =
     genericCreateVisitorTWorkerReactiveNetwork
         preforkVisitorWorkerThread
--- @+node:gcross.20111026213013.1279: *3* filterJust
-filterJust :: Event (Maybe a) → Event a
-filterJust = fmap fromJust . filterE isJust
 -- @+node:gcross.20111026172030.1277: *3* genericCreateVisitorTWorkerReactiveNetwork
 genericCreateVisitorTWorkerReactiveNetwork ::
     (
@@ -256,13 +231,5 @@ genericCreateVisitorTWorkerReactiveNetwork
         workerIncomingShutdownEvent
 
     return WorkerOutgoingEvents{..}
--- @+node:gcross.20111026213013.1281: *3* newHandler
-newHandler = do
-    (event_handler,callback) ← liftIO newAddHandler
-    event ← fromAddHandler event_handler
-    return (event,callback)
--- @+node:gcross.20111026213013.1282: *3* split
-split :: Event (Either a b) → (Event a,Event b)
-split = (fmap fromLeft . filterE isLeft) &&& (fmap fromRight . filterE isRight)
 -- @-others
 -- @-leo
