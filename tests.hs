c8e0b24892869d76dcff0ecff8519fca1e43cbc8
Gregory Crosswhite <gcrosswhite@gmail.com>
2012-09-13 12:02:14 +1000

    Tweaked status update test to use two workers intead of one.

--- tests.hs
+++ tests.hs
@@ -704,19 +704,20 @@ tests = -- {{{
                  ) >>= (@?= (VisitorNetworkResult (Left (VisitorStatusUpdate Unexplored ())) ([]::[()])))
                 readIORef maybe_status_update_ref >>= (@?= Just (VisitorStatusUpdate Unexplored ()))
              -- }}}
-            ,testCase "request status update when one worker present leaves" $ do -- {{{
+            ,testCase "request status update when active worker present leaves" $ do -- {{{
                 (maybe_status_update_ref,actions1) ← addReceiveCurrentStatusAction bad_test_supervisor_actions
                 (broadcast_ids_list_ref,actions2) ← addAppendStatusUpdateBroadcastIdsAction actions1
-                let actions3 = ignoreAcceptWorkloadAction actions2
+                let actions3 = ignoreAcceptWorkloadAction . ignoreWorkloadStealAction $ actions2
                 let status_update = VisitorStatusUpdate Unexplored (Sum 0)
                 (runVisitorNetworkSupervisor actions3 $ do
-                    updateWorkerAdded ()
+                    updateWorkerAdded True
+                    updateWorkerAdded False
                     requestStatusUpdate
-                    updateWorkerRemoved ()
+                    updateWorkerRemoved True
                     abortNetwork
-                 ) >>= (@?= (VisitorNetworkResult (Left status_update)) [])
+                 ) >>= (@?= (VisitorNetworkResult (Left status_update)) [False])
                 readIORef maybe_status_update_ref >>= (@?= Just status_update)
-                readIORef broadcast_ids_list_ref >>= (@?= [[()]])
+                readIORef broadcast_ids_list_ref >>= (@?= [[True]])
              -- }}}
             ,testCase "request and receive Nothing status update when one worker present" $ do -- {{{
                 (maybe_status_update_ref,actions1) ← addReceiveCurrentStatusAction bad_test_supervisor_actions
