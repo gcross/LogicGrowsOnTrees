e9a8bc6d625189b437477564d8839c9fbdba6659
Gregory Crosswhite <gcrosswhite@gmail.com>
2012-12-11 17:07:25 +1000

    Added a benchmark that uses artificial delays.

---
+++ benchmarks/run-versus-cpus-with-delays.hs
@@ -0,0 +1,41 @@
+-- Language extensions {{{
+{-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE ViewPatterns #-}
+-- }}}
+
+-- Imports {{{
+import Control.Concurrent
+import Control.Concurrent.MVar
+import Control.Exception
+import Control.Monad
+import Control.Monad.IO.Class
+
+import Data.Bits
+import Data.Monoid
+
+import Control.Monad.Trans.Visitor (VisitorIO)
+import Control.Monad.Trans.Visitor.Parallel.Threads
+
+import System.Log.Logger
+
+import Criterion.Main
+-- }}}
+
+sumtree :: Int → VisitorIO (Sum Int)
+sumtree 0 = liftIO (threadDelay 4) >> return (Sum 1)
+sumtree n = liftIO (threadDelay 4) >> (sumtree (n-1) `mplus` sumtree (n-1))
+
+runWithCPUs :: Int → Int → IO ()
+runWithCPUs depth number_of_workers = do
+    result_mvar ← newEmptyMVar
+    controller ← runVisitorIO (putMVar result_mvar) (sumtree depth)
+    changeNumberOfWorkersAsync (const (return number_of_workers)) controller (void . return)
+    takeMVar result_mvar >>= \x → case x of
+        Completed (getSum → result) → if result == shiftL 1 depth then return () else error $ "result == " ++ show result ++ ", but should be " ++ show (shiftL (1::Int) depth)
+        Aborted progress → error $ "aborted with progress " ++ show progress
+        Failure exception → throwIO exception
+    return ()
+
+main = defaultMain $ map (b 15) [1..4]
+  where
+    b depth number_of_workers = bench (show number_of_workers ++ " workers") (runWithCPUs depth number_of_workers)
