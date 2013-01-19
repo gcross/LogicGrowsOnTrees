21f7cd4a28aff1609a2450eb18b4d02023a4e92a
Gregory Crosswhite <gcrosswhite@gmail.com>
2013-01-19 18:22:33 +1100

    Wrote parallel version of the n-queens solution counter.

---
+++ count-all-nqueens-solutions-parallel.hs
@@ -0,0 +1,32 @@
+-- Language extensions {{{
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE UnicodeSyntax #-}
+-- }}}
+
+-- Imports {{{
+import Control.Concurrent.MVar
+import Control.Monad
+
+import Data.Functor
+
+import GHC.Conc
+
+import System.Environment
+import System.Log.Logger
+
+import Control.Monad.Trans.Visitor (IntSum(..))
+import Control.Monad.Trans.Visitor.Examples.Queens
+import Control.Monad.Trans.Visitor.Parallel.Threads
+-- }}}
+
+main = do
+    -- updateGlobalLogger rootLoggerName (setLevel DEBUG) 
+    n ← read . head <$> getArgs
+    number_of_workers ← getNumCapabilities
+    result_mvar ← newEmptyMVar
+    controller ← runVisitor (putMVar result_mvar) (nqueensCount n)
+    changeNumberOfWorkersAsync (const (return number_of_workers)) controller (void . return)
+    takeMVar result_mvar >>= \x → case x of
+        Completed (getIntSum → result) → print result
+        Aborted progress → error $ "aborted with progress " ++ show progress
+        Failure message → error message
