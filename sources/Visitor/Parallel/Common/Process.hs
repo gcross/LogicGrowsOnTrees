826b5d8742990e4b197c83271ffb8a59828a1c58
Gregory Crosswhite <gcrosswhite@gmail.com>
2013-06-24 14:18:10 +1000

    Documented the Process module.

--- sources/Visitor/Parallel/Common/Process.hs
+++ sources/Visitor/Parallel/Common/Process.hs
@@ -1,15 +1,23 @@
--- Language extensions {{{
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE UnicodeSyntax #-}
--- }}}
 
+{-| This module contains functions that let one easily implement the worker side
+    of a back-end under the assumption that the worker uses a two-way
+    communication channel with the supervisor for sending and receiving
+    messages.  (Examples of when this is NOT the case is the threads back-end,
+    where you can communicate with the worker threads directly, and the MPI
+    back-end, which has communication primitives that don't quite align with
+    this setup.)
+
+    Note:  This module is used by the processes and network back-end, which are
+           provided in separate packages.
+ -}
 module Visitor.Parallel.Common.Process
     ( runWorker
     , runWorkerUsingHandles
     ) where
 
--- Imports {{{
 import Control.Concurrent (killThread)
 import Control.Concurrent.MVar (isEmptyMVar,newEmptyMVar,putMVar,takeMVar,tryTakeMVar)
 import Control.Exception (AsyncException(ThreadKilled,UserInterrupt),catchJust)
@@ -27,23 +35,29 @@ import Visitor.Parallel.Common.Message (MessageForSupervisor(..),MessageForWorke
 import Visitor.Parallel.Common.Worker hiding (ProgressUpdate,StolenWorkload)
 import Visitor.Utils.Handle
 import Visitor.Workload
--- }}}
 
--- Logging Functions {{{
+--------------------------------------------------------------------------------
+----------------------------------- Loggers ------------------------------------
+--------------------------------------------------------------------------------
+
 deriveLoggers "Logger" [DEBUG,INFO]
--- }}}
 
--- Functions {{{
+--------------------------------------------------------------------------------
+----------------------------------- Functions ----------------------------------
+--------------------------------------------------------------------------------
 
-runWorker :: -- {{{
-    IO MessageForWorker →
-    (MessageForSupervisor ip fp → IO ()) →
+{-| Runs a loop that continually fetches and reacts to messages from the
+    supervisor until the worker quits.
+ -}
+runWorker ::
+    IO MessageForWorker {-^ the action used to fetch the next message -} →
+    (MessageForSupervisor progress worker_final_progress → IO ()) {-^ the action to send a message to the supervisor;  note that this might occur in a different thread from the worker loop -} →
     (
-        (WorkerTerminationReason fp → IO ()) →
+        (WorkerTerminationReason worker_final_progress → IO ()) →
         Workload →
-        IO (WorkerEnvironment ip)
-    ) →
-    IO ()
+        IO (WorkerEnvironment progress)
+    ) {-^ code to fork a worker thread with the given termination handler and workload -} →
+    IO () {-^ an IO action that loops processing messages until it is quit, at which point it returns -}
 runWorker receiveMessage sendMessage forkVisitorWorkerThread =
     newEmptyMVar >>= \worker_environment_mvar →
     let processRequest sendRequest constructResponse =
@@ -96,20 +110,22 @@ runWorker receiveMessage sendMessage forkVisitorWorkerThread =
         )
         processNextMessage
         (const $ return ())
--- }}}
 
-runWorkerUsingHandles :: -- {{{
-    Serialize result ⇒
-    Handle →
-    Handle →
+{-| This function is the same as 'runWorker', but it lets you provide handles
+    through which the messages will be sent and received.  (Note that the
+    reading and writing handles might be the same.)
+ -}
+runWorkerUsingHandles ::
+    ( Serialize progress
+    , Serialize worker_final_progress
+    ) ⇒
+    Handle {-^ handle from which messages from the supervisor are read -} →
+    Handle {-^ handle to which messages to the supervisor are written -} →
     (
-        (WorkerTerminationReason result → IO ()) →
+        (WorkerTerminationReason worker_final_progress → IO ()) →
         Workload →
-        IO (WorkerEnvironment result)
-    ) →
-    IO ()
+        IO (WorkerEnvironment progress)
+    ) {-^ code to fork a worker thread with the given termination handler and workload -} →
+    IO () {-^ an IO action that loops processing messages until it is quit, at which point it returns -}
 runWorkerUsingHandles receive_handle send_handle =
     runWorker (receive receive_handle) (send send_handle)
--- }}}
-
--- }}}
