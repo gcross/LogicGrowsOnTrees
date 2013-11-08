824a802067d4a2078295985605f33004af2ce662
Gregory Crosswhite <gcrosswhite@gmail.com>
2013-11-08 17:46:59 +1000

    Since mconcat for () ignores its argument we need to use our own unit.

--- benchmarks/tree-versus-list-unit-tree.hs
+++ benchmarks/tree-versus-list-unit-tree.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE UnicodeSyntax #-}
 
 import Criterion.Main
+import Control.DeepSeq
 import Data.Monoid
+import Data.Serialize
 import System.Environment
 
 import LogicGrowsOnTrees
@@ -17,16 +19,34 @@ import LogicGrowsOnTrees.Parallel.Purity (Purity(Pure))
 
 makeTree x = perfectTree x 2 15
 
+-- We need to define our own version of () because the mconcat method for ()
+-- completely ignores the argument list and so bypasses the cost of adding the
+-- ()'s up.
+data MyUnit = MyUnit
+
+instance Serialize MyUnit where
+    put _ = put ()
+    get = return MyUnit
+
+instance Monoid MyUnit where
+    mempty = MyUnit
+    mappend x y = x `seq` y `seq` MyUnit
+    mconcat [] = MyUnit
+    mconcat (x:xs) = x `seq` mconcat xs
+
+instance NFData MyUnit where
+    rnf x = x `seq` ()
+
 main = defaultMain
-    [bench "list" $ nf (mconcat . makeTree) ()
-    ,bench "tree" $ nf (exploreTree . makeTree) ()
-    ,bench "tree w/ checkpointing" $ nf (exploreTreeStartingFromCheckpoint Unexplored . makeTree) ()
-    ,bench "tree using worker" $ exploreTreeGeneric AllMode Pure (makeTree ())
-    ,bench "tree using single thread (direct)" $ Threads.exploreTree (setNumberOfWorkers 1) (makeTree ())
+    [bench "list" $ nf (mconcat . makeTree) MyUnit
+    ,bench "tree" $ nf (exploreTree . makeTree) MyUnit
+    ,bench "tree w/ checkpointing" $ nf (exploreTreeStartingFromCheckpoint Unexplored . makeTree) MyUnit
+    ,bench "tree using worker" $ exploreTreeGeneric AllMode Pure (makeTree MyUnit)
+    ,bench "tree using single thread (direct)" $ Threads.exploreTree (setNumberOfWorkers 1) (makeTree MyUnit)
     ,bench "tree using single thread (main)" $
         withArgs ["-n1"] $
             simpleMainForExploreTree
                 Threads.driver
                 (const $ return ())
-                (makeTree ())
+                (makeTree MyUnit)
     ]
