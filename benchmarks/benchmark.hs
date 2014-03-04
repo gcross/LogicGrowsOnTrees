bde28f4e08047b2f58359664677b2988b95a5833
Gregory Crosswhite <gcrosswhite@gmail.com>
2014-03-04 02:02:32 -0800

    Now the benchmarks work properly.

--- benchmarks/benchmark.hs
+++ benchmarks/benchmark.hs
@@ -1,47 +1,54 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE UnicodeSyntax #-}
 
+import Prelude hiding (catch)
+
+import Control.Exception (SomeException,catch)
 import Criterion.Main
 import Data.Monoid
 import System.Environment
+import System.IO
 
 import LogicGrowsOnTrees
 import LogicGrowsOnTrees.Checkpoint
-import LogicGrowsOnTrees.Utils.PerfectTree
+import LogicGrowsOnTrees.Utils.PerfectTree (trivialPerfectTree)
 import LogicGrowsOnTrees.Utils.WordSum
 import qualified LogicGrowsOnTrees.Parallel.Adapter.Processes as Processes
 import qualified LogicGrowsOnTrees.Parallel.Adapter.Threads as Threads
 import LogicGrowsOnTrees.Parallel.Adapter.Threads (setNumberOfWorkers)
 import LogicGrowsOnTrees.Parallel.Common.Worker (exploreTreeGeneric)
-import LogicGrowsOnTrees.Parallel.ExplorationMode (ExplorationMode(AllMode))
+import LogicGrowsOnTrees.Parallel.ExplorationMode (AllMode,ExplorationMode(AllMode))
 import LogicGrowsOnTrees.Parallel.Main
 import LogicGrowsOnTrees.Parallel.Purity (Purity(Pure))
 
-main = defaultMain
+depth = 15
+
+main = do
+    args ← getArgs
+    case args of
+        ["worker-bee"] →
+            Processes.runWorkerUsingHandles
+                AllMode
+                Pure
+                (trivialPerfectTree 2 depth)
+                stdin
+                stdout
+             `catch`
+             (\(e::SomeException) → error $ "Worker process failed: " ++ show e)
+        _ → Processes.getProgFilepath >>= defaultMain . benchmarks
+
+benchmarks worker_filepath =
     [bench "list" $ nf (getWordSum . mconcat . trivialPerfectTree 2) depth
     ,bench "tree" $ nf (getWordSum . exploreTree . trivialPerfectTree 2) depth
     ,bench "tree w/ checkpointing" $ nf (getWordSum . exploreTreeStartingFromCheckpoint Unexplored . trivialPerfectTree 2) depth
     ,bench "tree using worker" $ exploreTreeGeneric AllMode Pure (trivialPerfectTree 2 depth)
-    ,bench "tree using single thread (direct)" $ Threads.exploreTree (setNumberOfWorkers 1) (trivialPerfectTree 2 depth)
-    ,bench "tree using single thread (main)" $
-        withArgs ["-n1"] $
-            simpleMainForExploreTree
-                Threads.driver
-                (const $ return ())
-                (trivialPerfectTree 2 depth)
-    ,bench "tree using single process (direct)" $
-        Processes.runExplorer
-            (const AllMode)
-            Pure
-            (return ((),()))
+    ,bench "tree using single thread" $ Threads.exploreTree (setNumberOfWorkers 1) (trivialPerfectTree 2 depth)
+    ,bench "tree using single process" $
+        Processes.runSupervisor
+            (AllMode :: ExplorationMode (AllMode WordSum))
+            worker_filepath
+            ["worker-bee"]
             (const $ return ())
-            (const $ trivialPerfectTree 2 depth)
-            (\_ _ → return mempty)
-            (\_ _ → setNumberOfWorkers 1)
-    ,bench "tree using single process (main)" $
-        withArgs ["-n1"] $
-            simpleMainForExploreTree
-                Processes.driver
-                (const $ return ())
-                (trivialPerfectTree 2 depth)
+            mempty
+            (setNumberOfWorkers 1)
     ]
-  where depth = 15
