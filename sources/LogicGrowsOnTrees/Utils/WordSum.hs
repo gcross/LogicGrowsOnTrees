6c45ffe0638d7a49a0b4d5f694845786cf86c418
Gregory Crosswhite <gcrosswhite@gmail.com>
2019-07-18 16:14:16 -0400

    Got WordSum to compile.

---
+++ sources/LogicGrowsOnTrees/Utils/WordSum.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE UnicodeSyntax #-}
+
+{-| This module contains a type that specializes the 'Sum' 'Monoid' to 'Word'. -}
+module LogicGrowsOnTrees.Utils.WordSum where
+
+import Data.Monoid (Monoid(..))
+import Data.Serialize (Serialize(..))
+import Data.Typeable (Typeable)
+import Data.Word (Word)
+
+{-| An unpacked 'Word' whose 'Monoid' instance is addition. -}
+data WordSum = WordSum { getWordSum :: {-# UNPACK #-} !Word } deriving (Eq,Show,Typeable)
+
+{-| This instance sums the two contained 'Word's. -}
+instance Semigroup WordSum where
+    WordSum x <> WordSum y = WordSum (x+y)
+
+{-| This instance has the value 0. -}
+instance Monoid WordSum where
+    mempty = WordSum 0
+
+{-| This instance is equivalent to the instance for 'Word'. -}
+instance Serialize WordSum where
+    put = put . getWordSum
+    get = fmap WordSum get
+
+
+
