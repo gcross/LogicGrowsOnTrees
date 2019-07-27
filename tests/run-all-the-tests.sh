37de5cb026e7738beaf3d2aebbd0065916cde81f
Gregory Crosswhite <gcrosswhite@gmail.com>
2019-07-27 13:14:21 -0400

    Added script to run all of the tests.

---
+++ tests/run-all-the-tests.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+stack build :test-Checkpoint :test-Examples :test-Location :test-LogicGrowsOnTrees :test-Path :test-Path :test-RequestQueue :test-Supervisor :test-Threads :test-Utils :test-Worker :test-helpers :test-nqueens
