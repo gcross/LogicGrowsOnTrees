f9b7e65bf5b91083effc22d097bcd824379137e3
Gregory Crosswhite <gcrosswhite@gmail.com>
2019-07-27 16:06:34 -0400

    Got print-all-nqueens-solutions to work.

---
+++ examples/print-all-nqueens-solutions.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE UnicodeSyntax #-}
+
+import qualified Data.Foldable as Fold
+import Data.List (sort)
+import qualified Data.Sequence as Seq
+import Options.Applicative (fullDesc,progDesc)
+
+import LogicGrowsOnTrees.Parallel.Main
+import LogicGrowsOnTrees.Parallel.Adapter.Threads
+
+import LogicGrowsOnTrees.Examples.Queens
+
+main :: IO ()
+main =
+    mainForExploreTree
+        driver
+        boardSizeArgument
+        (fullDesc <> progDesc "print all the n-queens solutions for a given board size")
+        (\_ (RunOutcome _ termination_reason) → do
+            case termination_reason of
+                Aborted _ → error "search aborted"
+                Completed solutions → Fold.mapM_ print . Seq.unstableSort $ solutions
+                Failure _ message → error $ "error: " ++ message
+        )
+        (fmap (Seq.singleton . sort) . nqueensSolutions)
+
