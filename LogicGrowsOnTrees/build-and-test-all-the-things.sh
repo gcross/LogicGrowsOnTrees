c664dc7c9178c16f5260aadb49bf6779a0064e84
Gregory Crosswhite <gcrosswhite@gmail.com>
2019-07-29 16:14:26 -0400

    tutorial flag --> tutorials, flags default to False, added package to targs

--- LogicGrowsOnTrees/build-and-test-all-the-things.sh
+++ LogicGrowsOnTrees/build-and-test-all-the-things.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-stack build `cat tests/targets.lst` `cat examples/targets.lst` `cat tutorial/targets.lst` `cat benchmarks/targets.lst`
+stack build --flag LogicGrowsOnTrees:examples --flag LogicGrowsOnTrees:tutorials `cat tests/targets.lst` `cat examples/targets.lst` `cat tutorial/targets.lst` `cat benchmarks/targets.lst`
