dcfe7d96feb3c47930a0d31f4fe237644caaa72f
Gregory Crosswhite <gcrosswhite@gmail.com>
2019-07-30 08:19:48 -0400

    Got the benchmarks to work.

--- LogicGrowsOnTrees-processes/benchmarks/benchmark.hs
+++ LogicGrowsOnTrees-processes/benchmarks/benchmark.hs
@@ -1,11 +1,8 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE UnicodeSyntax #-}
 
-import Prelude hiding (catch)
-
 import Control.Exception (SomeException,catch)
 import Criterion.Main
-import Data.Monoid
 import System.Environment
 import System.IO
 
@@ -18,11 +15,12 @@ import qualified LogicGrowsOnTrees.Parallel.Adapter.Threads as Threads
 import LogicGrowsOnTrees.Parallel.Adapter.Threads (setNumberOfWorkers)
 import LogicGrowsOnTrees.Parallel.Common.Worker (exploreTreeGeneric)
 import LogicGrowsOnTrees.Parallel.ExplorationMode (AllMode,ExplorationMode(AllMode))
-import LogicGrowsOnTrees.Parallel.Main
 import LogicGrowsOnTrees.Parallel.Purity (Purity(Pure))
 
+depth :: Word
 depth = 15
 
+main :: IO ()
 main = do
     args ← getArgs
     case args of
@@ -37,13 +35,16 @@ main = do
              (\(e::SomeException) → error $ "Worker process failed: " ++ show e)
         _ → Processes.getProgFilepath >>= defaultMain . benchmarks
 
+benchmarks :: String → [Benchmark]
 benchmarks worker_filepath =
     [bench "list" $ nf (getWordSum . mconcat . trivialPerfectTree 2) depth
     ,bench "tree" $ nf (getWordSum . exploreTree . trivialPerfectTree 2) depth
     ,bench "tree w/ checkpointing" $ nf (getWordSum . exploreTreeStartingFromCheckpoint Unexplored . trivialPerfectTree 2) depth
-    ,bench "tree using worker" $ exploreTreeGeneric AllMode Pure (trivialPerfectTree 2 depth)
-    ,bench "tree using single thread" $ Threads.exploreTree (setNumberOfWorkers 1) (trivialPerfectTree 2 depth)
-    ,bench "tree using single process" $
+    ,bench "tree using worker" $ nfIO $
+         exploreTreeGeneric AllMode Pure (trivialPerfectTree 2 depth)
+    ,bench "tree using single thread" $ nfIO $
+         Threads.exploreTree (setNumberOfWorkers 1) (trivialPerfectTree 2 depth)
+    ,bench "tree using single process" $ nfIO $
         Processes.runSupervisor
             (AllMode :: ExplorationMode (AllMode WordSum))
             worker_filepath
